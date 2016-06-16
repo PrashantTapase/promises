c30cbd6434dd4f22cc533adf842047fe14f7e33c
PrashantTapase <tapase.prashant@gmail.com>
2016-06-16 12:43:52 +0530

    Made functions for add and sub

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 var promise = require('bluebird')
 
-function mathsCal(inputParam1, inputParam2){
+function addFunction(inputParam1, inputParam2){
 
-    new promise(function(resolve, reject){
+    return new promise(function(resolve, reject){
             
         var addResult = inputParam1 + inputParam2;
 
         if(addResult !== '' && !isNaN(addResult)){
-            resolve(addResult)    
+            console.log('Add result ',addResult)
+            var params = [inputParam1, inputParam2, addResult]
+            resolve(params)
         }
         else{
-            reject('something went wrong')  
+            reject('something went wrong in addFunction')  
         }
         
-    }).then(function(addResult){
-        
-        console.log('Add result ',addResult)
-        
-        var subResult = inputParam1 - inputParam2;
-
-        if(subResult !== '' && !isNaN(subResult)){
-             return subResult
-        }
-        else{
-            return 'something went wrong'
-        }
-        
-    }).then(function(subResult){
-        console.log('Sub result ',subResult)
     })
-    
 }
 
-mathsCal(10, 5)
+function subFunction(params){
+
+    var subResult = params[0] - params[1];
+
+        if(subResult !== '' && !isNaN(subResult)){
+            console.log('Sub result ',subResult)
+            return params
+        }
+        else{
+            throw 'something went wrong in subFunction'
+        }
+}
+
+addFunction(2, 1)
+.then(subFunction)
+.catch(function(e){
+    console.log('Error Found ',e); //Catch any unexpected errors
+})
